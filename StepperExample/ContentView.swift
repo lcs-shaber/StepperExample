b8c102d7480840312ea6fe51a926b5f3a9900840
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 11:18:55 -0500

    Finished the app by adding a computed property and using string interpolation.

--- StepperExample/ContentView.swift
+++ StepperExample/ContentView.swift
@@ -13,6 +13,11 @@ struct ContentView: View {
     @State var base: Int = 1
     
     // MARK: Computed properties
+    
+    var squared: Int {
+        return base * base
+    }
+    
     var body: some View {
         VStack {
             
@@ -29,7 +34,7 @@ struct ContentView: View {
                 Text("=")
                     .font(.system(size: 96))
 
-                Text("25")
+                Text("\(squared)")
                     .font(.system(size: 96))
             }
                  
