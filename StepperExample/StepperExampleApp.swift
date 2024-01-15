ca9c240730e873105cbbeba15e37ed3a052c0d2e
Sophie Haber <SHaber@lcs.on.ca>
2024-01-15 11:05:35 -0500

    Initial Commit

---
+++ StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Sophie Haber on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
